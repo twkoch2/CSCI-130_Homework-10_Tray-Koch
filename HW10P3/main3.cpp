a7c5c7e80fe75131a07ae8e37b9a2affb82d08d8
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-10-31 17:34:20 +0000

    Setting up final output array.

--- HW10P3/main3.cpp
+++ HW10P3/main3.cpp
@@ -1,7 +1,7 @@
 /***********************************************************************
 Programmer: Tray Koch
 Date Modified: 10/27/2022
-Description: This program was written to solve the Kattis problem Greedily Increasing Subsequence. This code must take in a permutation of a list of values. Then, the GIS values must be calculated.
+Description: This program was written to solve the Kattis problem Greedily Increasing Subsequence. This code must take in a permutation of a list of values. Then, the GIS values must be calculated. The GIS is a list of ascending values from the initial permutation. 
 ***********************************************************************/
 
 #include <iostream>
@@ -11,6 +11,8 @@ using namespace std;
 
 //prototype functions
 void fillArray(int list[], int listSize);
+void printArray (const int list[], int listSize);
+int numberOfGIS (int list[], int listSize);
 
 int main()
 {
@@ -22,6 +24,17 @@ int main()
   const int N = a;
   int permutationA[N]; //array to store sequence
 
+  //fill array called permutationA
+  fillArray(permutationA, N);
+
+  //now search through array and create new array to compute GIS
+  int b; //will be used to find length of array to be outputted to the console
+  b = numberOfGIS(permutationA, N);
+  const int M = b;
+
+  //create an output array for the final 
+  
+
 }
 
 //user-defined functions
@@ -29,7 +42,7 @@ int main()
 //function to fill array
 void fillArray(int list[], int listSize)
 {
-  cout << "Enter" << listSize << " integers: "; //comment out for Kattis
+  //cout << "Enter" << listSize << " integers: "; //comment out for Kattis
 
   int index;
 
@@ -39,4 +52,37 @@ void fillArray(int list[], int listSize)
     }
 
   return;
+}
+
+//print an array to the console
+void printArray (int list[], int listSize)
+{
+  int index;
+
+  for (index = 0; index < listSize; index++)
+  {
+     cout << list[index] << " ";
+    
+  }
+ 
+  cout << endl;
+  return;
+}
+
+int numberOfGIS (int list[], int listSize) //modified indexLargestElement function
+{
+  int GISCount = 1; //first value displayed
+  int maxIndex = 0; 
+
+  for (int index = 1; index < listSize; index++) //index is 1 since 1st integer always printed
+  {
+    if (list[maxIndex] < list[index])
+    {
+      maxIndex = index;
+      GISCount++;
+    }
+      
+  }
+
+  return GISCount;
 }
