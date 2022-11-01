5a34f1aa48b6c21f644a463053a20b42f92ed641
twkoch <111785868+twkoch2@users.noreply.github.com>
2022-11-01 15:46:20 +0000

    Finished part B.

--- .ccls-cache/@home@runner@HW10-Tray-Koch/HW10P5@main5.cpp
+++ .ccls-cache/@home@runner@HW10-Tray-Koch/HW10P5@main5.cpp
@@ -11,9 +11,11 @@ Description: This program was written to solve the Bronson Chapter 10 Programmin
 16
 99
 34
-The first output is 33 because the command *(*val) pulls from the 0,0 position in the array. The next output is 16 because this *(*val+1) pulls from the position row 0, column 1. The next output is 99. This is because *(*(val+1) + 2) moves over 2 columns and moves down a row from row 0 to row 1. This pulls an answer from the location [1][2] which is 99. Fianlly, the final value is 34 because it pulls data from the [0][0] location of the array and adds 1 to the answer because of the +1 which is outside the parerntheses surrounding val.
 
+The first output is 33 because the command *(*val) pulls from the [0][0] position in the array. The next output is 16 because this operator, *(*val+1), pulls from the position row 0, column 1. The next output is 99. This is because *(*(val+1) + 2) moves over 2 columns (to row 2) and moves down a row from row 0 to row 1. This pulls an answer from the location [1][2] which is 99. Fianlly, the final value is 34 because the function pulls data from the [0][0] location of the array and adds 1 to the answer. This is because the +1 is outside the parerntheses surrounding val.
 */
+
+
 #include <iostream>
 using namespace std;
 
@@ -33,6 +35,12 @@ void arr(int (*val) [3])
   cout << endl << *(*val);
   cout << endl << *(*val + 1);
   cout << endl << *(*(val + 1) + 2);
-  cout << endl << *(*val) + 1;
+  cout << endl << *(*val) + 1 << endl;
+  
+  //part b (is the notation val[1][2] valid within the function?)
+  cout << "\nThe output of the val[1][2] in the function is arr() in this code is as follows: ";
+  cout << "val[1][2] = " << val[1][2];
+
+  cout << endl << "Since this notation returns an answer that would be expected when returning the value in the index [1][2] of the array, this notation must be valid in the function arr()." << endl;
   return;
 }
